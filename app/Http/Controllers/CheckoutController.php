70893adec345128c6fc9d52ef764dd577d55eb36
moetokitai <kitcyon0301softbank.ne.jp@gmai.com>
2020-02-28 14:52:37 +0530

    first commit

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stripe\Stripe;
+use Stripe\Charge;
+use Cart;
+use App\Mail\PurchaseSuccessful;
+use Illuminate\Support\Facades\Mail;
+
+
+class CheckoutController extends Controller
+{
+    public function index(){
+       return view('checkout');
+    }
+
+    public function payment(){
+        // Set your secret key. Remember to switch to your live secret key in production!
+        // See your keys here: https://dashboard.stripe.com/account/apikeys
+        Stripe::setApiKey('sk_test_YmmLa9At2dUotLESYmYsmhMs00XQSYHHs5');
+
+        $token=request()->stripeToken;
+
+        $charge=Charge::create([
+        'amount' => Cart::total()*1000,
+        'currency' => 'inr',
+        'description'=>'laravel stripe payment',
+        'source'=>$token,
+        ]);
+
+
+        Cart::destroy();
+
+        Mail::to(request()->stripeEmail)->send(new PurchaseSuccessful);
+        return redirect('/');
+            }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [
+'uses'=>'FrontEndController@index',
+'as'=>'index'
+]);
+
+Route::get('/single-product/{product}', [
+    'uses'=>'FrontEndController@singleProduct',
+    'as'=>'product.single'
+    ]);
+Route::get('/cart', [
+    'uses'=>'ShoppingController@cart',
+    'as'=>'cart'
+    ]);
+    
+Route::post('/cart/add', [
+        'uses'=>'ShoppingController@add_to_cart',
+        'as'=>'cart.add'
+        ]);
+        
+Route::get('/cart/rapid/add/{id}', [
+        'uses'=>'ShoppingController@rapid_add',
+        'as'=>'rapid.cart.add'
+         ]);     
+         
+         
+Route::get('/cart/incr/{id}/{qty}', [
+            'uses'=>'ShoppingController@incr',
+            'as'=>'cart.incr'
+             ]);        
+
+Route::get('/cart/decr/{id}/{qty}', [
+                'uses'=>'ShoppingController@decr',
+                'as'=>'cart.decr'
+                 ]);  
+
+Route::get('cart/delete/{id}', [
+    'uses'=>'ShoppingController@cart_delete',
+    'as'=>'cart.delete'
+     ]);  
+
+Route::get('/cart/checkout', [
+        'uses'=>'CheckoutController@index',
+        'as'=>'cart.checkout'
+        ]);
+Route::post('/cart/checkout', [
+     'uses'=>'CheckoutController@payment',
+     'as'=>'cart.checkout'
+     ]);           
+
+        
+            
+        
+
+
+Route::resource('product','ProductController');
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+
